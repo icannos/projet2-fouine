0082eec8feb8ff79f552b88f734a1b5028eafa07
Icannos <icannos.connect-radio@live.fr>
2018-03-02 09:58:37 +0100

    fix auto_tests.sh

--- auto_test.sh
+++ auto_test.sh
@@ -4,8 +4,8 @@
 # Ce script exécute parallèlement les scripts .ml avec fouine et ocaml puis effectue un diff des sorties
 #
 
-tests_dir="tests/"
-fouinepath = "./bin/fouine"
+tests_dir="tests"
+fouinepath=./bin/fouine
 
 echo -e "\n **************** Starting Tests *****************"
 for test in $(ls -p $tests_dir | grep -v /)
@@ -15,7 +15,7 @@ do
 
 		# On pipe les sorties des deux exécutions sur le diff
 		# On ajoute à la volée la définition de prInt pour qu' OCaml ne râle pas
-		output=$(diff <( $fouinepath $tests_dir/$test ) <( ocaml <(echo "let prInt x = print_int x;print_newline(); x;;" ; cat $tests_dir/$test) ) )
+		output=$(diff <($fouinepath $tests_dir/$test ) <( ocaml <(echo "let prInt x = print_int x;print_newline(); x;;" ; cat $tests_dir/$test) ) )
 
 		# on affiche ok s'il n'y a pas de problèmes, sinon on affiche la sortie du diff.
 		if [[ -z ${output//} ]]
