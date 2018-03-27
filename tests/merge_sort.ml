10c09700d83464fd004026f5336bc43dd1903455
Maxime <maxime.darrin@ens-lyon.fr>
2018-03-27 21:03:00 +0200

    tests + fix

--- tests/merge_sort.ml
+++ tests/merge_sort.ml
@@ -1,3 +1,8 @@
+let rec print_list l = match l with
+|[] -> ()
+|x::q -> prInt x; print_list q
+;;
+
 let rec fusion la lb =
   match (la, lb) with
   |(x::q, []) -> x::q
@@ -5,4 +10,6 @@ let rec fusion la lb =
   |(x::q, y::t) -> if x < y then x::(fusion q lb) else y::(fusion la t)
   ;;
 
-  fusion ([1;2;8;6;9]) ([1;2;3;4;5;6;7;8;9])
+
+
+  print_list (fusion ([1;2;8;6;9]) ([1;2;3;4;5;6;7;8;9]))
