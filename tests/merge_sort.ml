b7633553c6e1c0ca04becaee8d997758183a551f
Maxime <maxime.darrin@ens-lyon.fr>
2018-03-27 21:39:09 +0200

    tests

--- tests/merge_sort.ml
+++ tests/merge_sort.ml
@@ -10,6 +10,16 @@ let rec fusion la lb =
   |(x::q, y::t) -> if x < y then x::(fusion q lb) else y::(fusion la t)
   ;;
 
+let rec split l = match l with
+|[] -> ([],[])
+|[x] -> ([x], [])
+|x::y::q -> let (la, lb) = split q in (x::la, y::lb)
+;;
 
+let rec merge_sort l = match l with
+|[] -> []
+|[x] -> [x]
+|t -> let (la, lb) = split t in (fusion (merge_sort la) (merge_sort lb))
+;;
 
-  print_list (fusion ([1;2;8;6;9]) ([1;2;3;4;5;6;7;8;9]))
+print_list (merge_sort ([1;2;8;6;9;1;2;3;4;5;6;7;8;9]))
