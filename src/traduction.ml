74efe5b4e050f9d81a4eb67a8494aa23df49682e
Maxime <maxime.darrin@ens-lyon.fr>
2018-04-09 22:38:35 +0200

    one more fix

--- src/traduction.ml
+++ src/traduction.ml
@@ -22,8 +22,6 @@ let rec trad_expr ee env =
       | Let((x, e1), e2) -> let e1 = trad_expr e1 and e2 = trad_expr e2 in
       (0, Fun("s",(0,Let( ((0,Cart([(0,Identifier "x");(0,Identifier "s1")])), (0,App((0,Identifier "e1"), (0,Identifier "s")))), ((0,App((0,Identifier "e2"), (0,Identifier "s1"))))))))
 
-      | LetRec((x, e1), e2) -> let e1 = trad_expr e1 and e2 = trad_expr e2 in
-      (0, Fun("s",(0,LetRec( ((0,Cart([(0,Identifier "x");(0,Identifier "s1")])), (0,App((0,Identifier "e1"), (0,Identifier "s")))), ((0,App((0,Identifier "e2"), (0,Identifier "s1"))))))))
       | App(e1,e2) -> let e1 = trad_expr e1 and e2 = trad_expr e2 in (0,Let( ((0,Identifier "_"), (0,Let( ((0,Identifier "e1"), (0, Fun("x",(0,Identifier "x")))), ((0,Let( ((0,Identifier "e2"), (0, Fun("x",(0,Identifier "x")))), ((0, Fun("s",(0,Let( ((0,Cart([(0,Identifier "f1");(0,Identifier "s1")])), (0,App((0,Identifier "e1"), (0,Identifier "s")))), ((0,Let( ((0,Cart([(0,Identifier "v2");(0,Identifier "s2")])), (0,App((0,Identifier "e2"), (0,Identifier "s1")))), ((0,App((0,App((0,Identifier "f1"), (0,Identifier "v2"))), (0,Identifier "s2")))))))))))))))))), ((0, Const 0))))
 
 
@@ -36,13 +34,16 @@ let rec trad_expr ee env =
       (0,Let( ((0,Identifier "_"), (0,Let( ((0,Identifier "e"), (0, Fun("x",(0,Identifier "x")))), ((0,Let( ((0,Identifier "read"), (0,Uni)), ((0, Fun("s",(0,Let( ((0,Cart([(0,Identifier "l");(0,Identifier "s1")])), (0,App((0,Identifier "e"), (0,Identifier "s")))), ((0,Let( ((0,Identifier "v"), (0,App((0,App((0,Identifier "read"), (0,Identifier "l"))), (0,Identifier "s1")))), ((0,Cart([(0,Identifier "v");(0,Identifier "s1")]))))))))))))))))), ((0, Const 0))))
 
 
-      | Aff(nom, e1)-> let nom = trad_expr nom in let e1 = trad_expr e1 in (0,Let( ((0,Identifier "_"), (0, Fun("s0",(0,Let( ((0,Cart([(0,Identifier "v1");(0,Identifier "s1")])), (0,App((0,Identifier "e1"), (0,Identifier "s0")))), ((0,Let( ((0,Cart([(0,Identifier "v2");(0,Identifier "s2")])), (0,App((0,Identifier "e2"), (0,Identifier "s1")))), ((0,Let( ((0,Identifier "s3"), (0,App((0,App((0,Identifier "modify"), (0,Identifier "s2"))), (0,Cart([(0,Identifier "v1");(0,Identifier "v2")]))))), ((0,Cart([(0,Uni);(0,Identifier "s3")])))))))))))))), ((0, Const 0))))
+      | Aff(nom, e1)-> let e1 = trad_expr e1 in (0,Let( ((0,Identifier "_"), (0, Fun("s0",(0,Let( ((0,Cart([(0,Identifier "v1");(0,Identifier "s1")])), (0,App((0,Identifier "e1"), (0,Identifier "s0")))), ((0,Let( ((0,Cart([(0,Identifier "v2");(0,Identifier "s2")])), (0,App((0,Identifier "e2"), (0,Identifier "s1")))), ((0,Let( ((0,Identifier "s3"), (0,App((0,App((0,Identifier "modify"), (0,Identifier "s2"))), (0,Cart([(0,Identifier "v1");(0,Identifier "v2")]))))), ((0,Cart([(0,Uni);(0,Identifier "s3")])))))))))))))), ((0, Const 0))))
 
       | Ref(e) ->let e = trad_expr e in (0, Fun("s",(0,Let( ((0,Cart([(0,Identifier "v");(0,Identifier "s1")])), (0,App((0,Identifier "e"), (0,Identifier "s")))), ((0,Let( ((0,Cart([(0,Identifier "l");(0,Identifier "s2")])), (0,App((0,App((0,Identifier "allocate"), (0,Identifier "v"))), (0,Identifier "s1")))), ((0,Cart([(0,Identifier "l");(0,Identifier "s2")]))))))))))
 
 
+      | LetRec((x, e1), e2) -> let e1 = trad_expr e1 and e2 = trad_expr e2 in
+      (0, Fun("s",(0,LetRec( ((0,Cart([(0,Identifier "x");(0,Identifier "s1")])), (0,App((0,Identifier "e1"), (0,Identifier "s")))), ((0,App((0,Identifier "e2"), (0,Identifier "s1"))))))))
 
-      | x -> (0, Fun("s",(0,Cart((0,x),(0,Identifier "s")))))
+      | x -> (0, Fun("s",(0,Cart([(0,x);(0,Identifier "s")]))))
+
 
 
   with x -> error_display node_id x; raise Fail
