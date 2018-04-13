48a685ad45582cd1c8aba9cc05f00683eab69b7c
Maxime <maxime.darrin@ens-lyon.fr>
2018-04-13 11:27:22 +0200

    meta constr

--- src/constructeur.ml
+++ src/constructeur.ml
@@ -1,2 +1,12 @@
+
+(* f e *)
 let mkApp f e = (0, App(f, e));;
-(*  let mkLetPair (a,b,c,e) = à faire car très utilisé*)
+
+(* patt -> e *)
+let mkFun patt e = (0, Fun(patt, e));;
+
+(* let (a,b) = c in d *)
+let mkLetPair (a, b) c d = (0, Let(( (0, Cart[a;b]), c), d);;
+
+(* string -> Identifier string *)
+let mkIdentifier varname = (0, Identifier varname);;
