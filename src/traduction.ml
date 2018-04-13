c3fe9c324b4f2a43b3ba77c1aaf6f16339f6e6f9
Edwige Cyffers <edwige.cyffers@ens-lyon.fr>
2018-04-13 12:03:41 +0200

    une traduction moins fausse

--- src/traduction.ml
+++ src/traduction.ml
@@ -2,18 +2,18 @@ open Affichage;;
 open Expr;;
 open Env;;
 open Errmgr;;
-open Constucteur;
+open Constructeur;;
 
 (*deux références globales qui permettront d'éviter les conflits dans la traduction*)
-let ref nbv =0;;
-let ref nbs =0;;
+let  nbv =ref 0;;
+let  nbs =ref 0;;
 
 (*fonction qui renvoie un sn où n est un numéro par encore utilisé*)
 let news () =
   nbs := !nbs + 1;
  (0,Identifier "s" ^ (string_of_int nbs));;
   
-let news () =
+let newv () =
   nbv := !nbv + 1;
  (0,Identifier "v" ^ (string_of_int nbv));;
   
@@ -25,6 +25,15 @@ let rec trad_expr ee =
   let (node_id, e) = ee in
 
   try match e with
-      | Add(e1,e2) -> let  s0 = news () in let s1 = news() in let s2 = news() in  let v1 = newv () in let v2 = newv () in let te1 =  trad_expr e1 in let te2 = trad_expr e2 in
-     mkLetPair (v1,s1) mkApp(te1, s0) (mkLetPair(v2,s2) mkApp(te2, s1) mkPair(Add(v1,v2),s2))
+      | Add(e1,e2) -> let  s0 = news () in let s1 = news() in let s2 = news() in  let v1 = newv () in let v2 = newv () in let te1 =  trad_expr e1 in let te2 = trad_expr e2 in mkFun s0
+    ( mkLetPair (v1,s1) mkApp(te1, s0)
+     (mkLetPair(v2,s2) mkApp(te2, s1) mkPair(mkAdd(v1,v2),s2)))
+       | Mul(e1,e2) -> let  s0 = news () in let s1 = news() in let s2 = news() in  let v1 = newv () in let v2 = newv () in let te1 =  trad_expr e1 in let te2 = trad_expr e2 in mkFun s0
+    ( mkLetPair (v1,s1) mkApp(te1, s0)
+     (mkLetPair(v2,s2) mkApp(te2, s1) mkPair(mkMul(v1,v2),s2)))             | Sou(e1,e2) -> let  s0 = news () in let s1 = news() in let s2 = news() in  let v1 = newv () in let v2 = newv () in let te1 =  trad_expr e1 in let te2 = trad_expr e2 in mkFun s0
+    ( mkLetPair (v1,s1) mkApp(te1, s0)
+     (mkLetPair(v2,s2) mkApp(te2, s1) mkPair(mkSou(v1,v2),s2)))             | Div(e1,e2) -> let  s0 = news () in let s1 = news() in let s2 = news() in  let v1 = newv () in let v2 = newv () in let te1 =  trad_expr e1 in let te2 = trad_expr e2 in mkFun s0
+    ( mkLetPair (v1,s1) mkApp(te1, s0)
+    (mkLetPair(v2,s2) mkApp(te2, s1) mkPair(mkDiv(v1,v2),s2)))
+       | x -> let s = news () in mkFun s mkPair(x, s))                                                                                                                                                                                                                   
   with x -> error_display node_id x; raise Fail
