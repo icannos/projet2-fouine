f3535c19f3a897958af5397375ef2ac9a9011262
Maxime <maxime.darrin@ens-lyon.fr>
2018-04-14 13:47:35 +0200

    oups

--- src/traduction.ml
+++ src/traduction.ml
@@ -25,8 +25,8 @@ let rec trad_expr ee =
   try match e with
       | Const x -> let s0 = news () in mkFun s0 (mkPair ((mkConst x), s0))
       | Identifier x -> let s0 = news () in mkFun s0 (mkPair ((mkIdentifier x), s0))
-      | PrintInt e -> let s0 = news () and s1 = news () and v = newv () and te = trad_expr e in
-      mkFun s0 (mkLetPair (v1, s1) (mkApp te s0) (mkPair(mkPrintInt v1, s1))
+      | PrintInt e -> let s0 = news () and s1 = news () and v1 = newv () and te = trad_expr e in
+      mkFun s0 (mkLetPair (v1, s1) (mkApp te s0) (mkPair(mkPrintInt v1, s1)))
 
       | Add(e1,e2) -> let  s0 = news () in let s1 = news() in let s2 = news() in  let v1 = newv () in let v2 = newv () in let te1 =  trad_expr e1 in let te2 = trad_expr e2 in
       mkFun s0 ( mkLetPair (v1,s1) (mkApp te1 s0) (mkLetPair (v2,s2) (mkApp te2 s1) (mkPair ((mkAdd v1 v2),s2))))
