29465ccc5b2e00e189fbac56f208a0246aaa90d9
Maxime <maxime.darrin@ens-lyon.fr>
2018-05-01 11:51:27 +0200

    inquisition

--- src/expr.ml
+++ src/expr.ml
@@ -52,6 +52,9 @@ type expr =
   |Typed of extexpr
   |TypeId of name
 
+  (* Pure *)
+  |Pure of extexpr
+
  and bexpr =
   | Testeq of extexpr * extexpr
   | Testneq of extexpr * extexpr
@@ -128,3 +131,78 @@ and freevarsb bindedvars fvars ee =
   | Testget(e1,e2)  -> VarsSet.union (freevars bindedvars fvars e1) (freevars bindedvars fvars e2)
 
 ;;
+
+
+let rec inquisition ast =
+  let (node_id, e) = ast in
+  match e with
+  |Const x -> (0, Pure(ast))
+  |Constr(nom, le) -> let lp = List.map inquisition le in
+    (*if List.for_all (fun x -> match x with |(_, Pure(e)) -> true |_ -> false)) lp then  (0, Pure(ast))*)
+    (node_id, Constr(nom, lp))
+  |Cart le  ->let lp = List.map inquisition le in
+    (*if List.for_all (fun x -> match x with |(_, Pure(e)) -> true |_ -> false)) lp then  (0, Pure(ast))*)
+    (node_id, Cart(lp))
+
+  |Add(e1,e2) -> let c = (inquisition e1,inquisition e2) in
+    begin match c with
+    |((_, Pure(x)), (_, Pure(y))) -> (node_id, Pure(ast))
+    | (x,y) -> (node_id, Add(x,y))
+    end
+  |Sou(e1,e2) -> let c = (inquisition e1,inquisition e2) in
+    begin match c with
+    |((_, Pure(x)), (_, Pure(y))) -> (node_id, Pure(ast))
+    | (x,y) -> (node_id, Sou(x,y))
+    end
+  |Div(e1,e2) -> let c = (inquisition e1,inquisition e2) in
+    begin match c with
+    |((_, Pure(x)), (_, Pure(y))) -> (node_id, Pure(ast))
+    | (x,y) -> (node_id, Div(x,y))
+    end
+  |Mul(e1,e2) -> let c = (inquisition e1,inquisition e2) in
+    begin match c with
+    |((_, Pure(x)), (_, Pure(y))) -> (node_id, Pure(ast))
+    | (x,y) -> (node_id, Mul(x,y))
+    end
+
+    |Let(((0, Identifier (nom, t)), e1), e2) -> let c = (inquisition e1,inquisition e2) in
+      begin match c with
+      |((_, Pure(x)), (_, Pure(y))) -> (node_id, Pure(ast))
+      | (x,y) -> (node_id, Let(((node_id, Identifier (nom, t)), x), y))
+      end
+
+    |Let((patt, e1), e2) -> (node_id, Let((patt, inquisition e1), inquisition e2))
+
+
+
+    |Aff(e1, e2)  -> (node_id, Aff(inquisition e1, inquisition e2))
+    |Ref e  -> (node_id, Ref (inquisition e))
+    |Acc (e)  -> (node_id, Acc (inquisition e))
+
+
+    |Identifier (x, _) -> (node_id, Pure(ast))
+    |Uni -> ast
+    |Vide -> ast
+    |Liste(t,q) -> (node_id, Liste(inquisition t, inquisition q))
+
+    | Raise e -> (node_id, Raise(inquisition e))
+    | PrintInt e  -> (node_id, PrintInt(inquisition e))
+
+    | Cond(booleen,e1,e2)  -> (node_id, Cond(inquisition_bool booleen, inquisition e1, inquisition e2))
+    | Fun(pattern, expr)  -> (node_id, Fun(pattern, inquisition expr))
+
+    |Match(expr, listxpr)  ->  (node_id, Match(inquisition expr, List.map inquisition listxpr))
+    |PattCase(casexpr, expr)  -> (node_id, PattCase(casexpr, inquisition expr))
+
+    |Try(expr, listxpr)  ->  (node_id, Try(inquisition expr, List.map inquisition listxpr))
+
+    | App(e1, e2)  -> (node_id, App(inquisition e1, inquisition e2))
+  and inquisition_bool ee =
+    let (node_id, e) = ee in
+    match e with
+    | Testeq(e1,e2) -> (node_id, Testeq(inquisition e1, inquisition e2))
+    | Testneq(e1,e2) -> (node_id, Testneq (inquisition e1, inquisition e2))
+    | Testlt(e1,e2) -> (node_id, Testlt (inquisition e1, inquisition e2))
+    | Testgt(e1,e2) -> (node_id, Testgt (inquisition e1, inquisition e2))
+    | Testlet(e1,e2) -> (node_id, Testlet (inquisition e1, inquisition e2))
+    | Testget(e1,e2) -> (node_id, Testget (inquisition e1, inquisition e2))
