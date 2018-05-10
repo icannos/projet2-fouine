cbfa5cfb506cd1e5ff78111b1fb9e37d075eaba5
Maxime <maxime.darrin@ens-lyon.fr>
2018-05-10 14:51:45 +0200

    ca avance

--- src/typechecking.ml
+++ src/typechecking.ml
@@ -1,6 +1,6 @@
 open Expr
 open Env
-
+open Errmgr
 
 type f_type = Int_f
 | List_f of f_type (* On stocke le type d'élément stockés dans la liste *)
@@ -19,15 +19,24 @@ type env_type_t = f_type Environnement.t;;
 
 (* Décide si deux trucs peuvent avoir même type, et si oui renvoie le type
 convenant aux deux trucs, sinon explose*)
-let rec unify e_t1 e_t2 =
+let rec unify (e_t1 : f_type) (e_t2 : f_type) (type_list : type_list_t) =
+  let unify = fun x y -> unify x y type_list in
   match e_t1, e_t2 with
   |Free a, Free b -> Free a
+  |Free a, x |x, Free a -> x
+  |Fun_f(x1, y1), Fun_f(x2, y2) -> Fun_f(unify x1 x2 , unify y1 y2)
+  |Ref_f(x1), Ref_f(x2) -> Ref_f(unify x1 x2)
+  |List_f(x1), List_f(x2) -> List_f (unify x1 x2)
+  |UserType s, x |x, UserType s -> unify (type_list s) x
+  |Cart l1, Cart l2 when (List.length l1 = List.length l2) -> Cart (List.map2 unify l1 l2)
+
+  | _ -> raise (TypesDoNotMatch (string_of_type e_t1, string_of_type e_t1)
+
 ;;
 
 let rec infer ee (type_env : env_type_t) (type_list : type_list_t) =
   let (node_id, e) = ee in (* Le node_id est important ici pour râler concernant
     les types qui matchent pas *)
-
   match e with
   | Const x -> Int_f
   | Vide -> List_f (Free "'a'")
