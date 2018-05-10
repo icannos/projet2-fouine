087d80bb5b411c7f1f0b78b075ba5f9e158ab420
Maxime <maxime.darrin@ens-lyon.fr>
2018-05-10 13:14:06 +0200

    types

--- src/typechecking.ml
+++ src/typechecking.ml
@@ -1,12 +1,24 @@
 open Expr
+open Env
 
 
+type f_type = Int_f
+| List_f of f_type (* On stocke le type d'élément stockés dans la liste *)
+| Fun_f of f_type * f_type (* Une fonction fouine, de type truc donne bidule *)
+| Cart_f of f_type list (* Un produit cart c'est une liste de type*)
+| UserType of string (* Nom du type somme défini par le programmeur *)
+;;
 
+(* On stocke les types définis par l'utilisateur *)
+type type_list_t = f_type Environnement.t;;
 
+(* Pour stocker le type des variables *)
+type env_type_t = f_type Environnement.t;;
 
+let rec infer ee (type_env : env_type_t) (type_list : type_list_t) =
+  let (node_id, e) = ee in (* Le node_id est important ici pour râler concernant
+    les types qui matchent pas *)
 
-let rec infer ee t_env =
-  let (node_id, e) = ee in
   match e with
-  | patt -> expr
+  | Const x -> Int_f
   | _ -> expr2
