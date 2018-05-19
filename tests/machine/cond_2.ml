ed8abaffcf22ea1307f23580fe4f286fa96c3fbb
Maxime <maxime.darrin@ens-lyon.fr>
2018-05-19 12:14:35 +0200

    test conds

---
+++ tests/machine/cond_2.ml
@@ -0,0 +1,5 @@
+(if 5 > 3 then prInt 0 else prInt 1);
+(if 5 < 3 then prInt 1 else prInt 0);
+(if 5 >= 3 then prInt 0 else prInt 1);
+(if 5 <= 3 then prInt 1 else prInt 0);
+(if 5 = 3 then prInt 1 else prInt 0)
